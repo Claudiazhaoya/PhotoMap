f822cb1bcf54f59db0979aded090ce91267db3e0
Claudiazhaoya <sun515@purdue.edu>
2017-12-27 01:05:25 -0800

    photo image finished

---
+++ Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,98 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoMapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var cameraButton: UIButton!
+    var currImage: UIImage?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+        mapView.delegate = self
+        cameraButton.layer.cornerRadius = cameraButton.frame.height / 2.0
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        let des = segue.identifier
+        if des == "tagSegue" {
+            let vc = segue.destination as! LocationsViewController
+            vc.delegate = self
+            
+        } else if des == "fullImageSegue" {
+            let vc = segue.destination as! FullImageViewController
+            vc.image = sender as! UIImage
+        }
+    }
+}
+
+extension PhotoMapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "myAnnotationView"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView!.canShowCallout = true
+            annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+        }
+        
+        let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
+        imageView.image = UIImage(named: "camera")
+        
+        return annotationView
+}
+}
+
+
+extension PhotoMapViewController: LocationsViewControllerDelegate {
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+        let locationCoordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
+        
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationCoordinate
+        annotation.title = "Picture!"
+        mapView.addAnnotation(annotation)
+    }
+}
+
+extension PhotoMapViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            picker.dismiss(animated: true, completion: {
+                self.currImage = image
+                self.performSegue(withIdentifier: "tagSegue", sender: nil)
+            })
+        }
+    }
+}
